0b68935fbb316a9bce8cfd4f9733733dfcf9de1e
Omkar Ghodechor <ghodechoromkar@gmail.com>
2025-10-22 13:24:46 +0530

    new project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiBaseUrl1: 'https://broadsidejumpersrv.emails.services/solr',
+    apiBaseUrl2: 'https://broadsidejumper2srv.emails.services/solr',
+    apiBaseUrl3: 'https://broadsidejumper3srv.emails.services/solr'
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    apiBaseUrl1: '/solr1',
+    apiBaseUrl2: '/solr2',
+    apiBaseUrl3: '/solr3'
+  };
+  
